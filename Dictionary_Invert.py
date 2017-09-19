e969c126468980819553a280e9c0e9750d5a57c5
jon-drew <drewnowskyj@gmail.com>
2017-09-18 20:08:26 -0400

    Update Dictionary_Invert.py

--- Dictionary_Invert.py
+++ Dictionary_Invert.py
@@ -1,10 +1,7 @@
 def dict_invert(d):
-    '''
-    d: dict
-    Returns an inverted dictionary according to the instructions above
-    '''
+'''Returns a dictionary with the key-value pairings reversed'''
     invert_dict = {}
-    for key, value in d.items():
+    for key, value in dict.items():
         invert_dict[value] = sorted(invert_dict.get(value, []))
         invert_dict[value].append(key)
     return invert_dict
