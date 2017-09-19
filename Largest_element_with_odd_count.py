9a7ac8d7206a42a14b8109a7dbf5d61e53bfe8f9
jon-drew <drewnowskyj@gmail.com>
2017-09-18 20:15:16 -0400

    Update Largest_element_with_odd_count.py

--- Largest_element_with_odd_count.py
+++ Largest_element_with_odd_count.py
@@ -7,14 +7,14 @@ def biggest(listToMeasure):
             current_answer = element
     return current_answer
 
-def largest_odd_times(L):
-    """ Assumes L is a non-empty list of ints
-        Returns the largest element of L that occurs an odd number 
-        of times in L. If no such element exists, returns None """
+def largest_odd_times(X):
+    """ Assumes X is a non-empty list of ints
+        Returns the largest element of X that occurs an odd number 
+        of times in X. If no such element exists, returns None """
     checked_elements = []
     elements_with_odd_counts = []
-    for element in L:
-        if L.count(element) % 2 != 0 and element not in checked_elements:
+    for element in X:
+        if X.count(element) % 2 != 0 and element not in checked_elements:
             checked_elements.append(element)
             elements_with_odd_counts.append(element)
     return biggest(elements_with_odd_counts)
