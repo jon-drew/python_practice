f169c8eef9906206e1c09848fc1bb00871fcb740
jon-drew <drewnowskyj@gmail.com>
2017-09-18 20:14:06 -0400

    Update Guess_your_number.py

--- Guess_your_number.py
+++ Guess_your_number.py
@@ -6,7 +6,8 @@ print ('Please think of a number between 0 and 100!')
 while correct == False:
     currentGuess = (min+max)//2
     print ('Is your secret number ' + str(currentGuess) + '?')
-    highLowInput = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+    highLowInput = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low."
+                        " Enter 'c' to indicate I guessed correctly.")
     if highLowInput == 'c':
         correct = True
         print ('Game over. Your secret number was: ' + str(currentGuess))
